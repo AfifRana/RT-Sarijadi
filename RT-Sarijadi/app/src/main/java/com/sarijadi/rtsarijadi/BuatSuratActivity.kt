89c233cd7142bc7a04757a273e0945af73234cdf
AfifRana <36437450+AfifRana@users.noreply.github.com>
2021-01-19 21:06:10 +0700

    init

---
+++ RT-Sarijadi/app/src/main/java/com/sarijadi/rtsarijadi/BuatSuratActivity.kt
@@ -0,0 +1,11 @@
+package com.sarijadi.rtsarijadi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BuatSuratActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buat_surat)
+    }
+}
---
+++ RT-Sarijadi/app/src/main/java/com/sarijadi/rtsarijadi/CatatanKeuanganActivity.kt
@@ -0,0 +1,11 @@
+package com.sarijadi.rtsarijadi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CatatanKeuanganActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catatan_keuangan)
+    }
+}
